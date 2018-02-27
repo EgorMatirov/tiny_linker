c6c72cf4eefcc1b27baf9179a95aacc8824c9634
Mikhail Ivchenko <ematirov@gmail.com>
2018-02-27 16:28:21 +0400

    Add linker test

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,7 +1,8 @@
 #include <tiny_linker/ObjectFile.h>
-#include <gtest/gtest.h>
+#include <tiny_linker/Linker.h>
+#include <tiny_linker/ExecutableFile.h>
 
-#include <fstream>
+#include <gtest/gtest.h>
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
@@ -61,4 +62,42 @@ TEST(object_file, read_relocations_correctly) {
 
     EXPECT_EQ(objectFile.GetRelocations()[0]->getSymbol(), 3);
     EXPECT_EQ(objectFile.GetRelocations()[1]->getSymbol(), 4);
+}
+
+TEST(linker, link_correctly_when_main_is_first) {
+    const std::vector<std::string> objectFilePaths = {
+            "../../tests/data/1/input/helloworld.o",
+            "../../tests/data/1/input/pri.o"
+    };
+
+    const auto expectedFilePath = "../../tests/data/1/expected_output";
+    std::shared_ptr<std::ifstream> expected = std::make_shared<std::ifstream>(expectedFilePath, std::ifstream::binary);
+
+    std::vector<std::shared_ptr<tiny_linker::ObjectFile>> objectFiles;
+
+    // Загружаем объектные файлы и парсим их
+    for (const auto &objectFilePath : objectFilePaths) {
+        std::shared_ptr<std::ifstream> dataStream = std::make_shared<std::ifstream>(objectFilePath,
+                                                                                    std::ifstream::binary);
+        std::shared_ptr<tiny_linker::ObjectFile> objectFile = std::make_shared<tiny_linker::ObjectFile>(dataStream);
+        objectFiles.push_back(objectFile);
+    }
+
+    // Линкуем.
+    tiny_linker::Linker linker;
+    auto executableFile = linker.Link(objectFiles);
+
+    std::stringstream result(std::ios_base::in | std::ios_base::out | std::ios_base::binary);
+    executableFile->Write(result);
+
+    result.seekg(0);
+
+    while (!result.eof() && !expected->eof()) {
+        auto resultInt = result.get();
+        auto expectedInt = expected->get();
+
+        EXPECT_EQ(resultInt, expectedInt);
+    }
+
+    EXPECT_EQ(result.eof(), expected->eof());
 }
